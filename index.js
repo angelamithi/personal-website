421db5dd7405fd5d8a6c3f3d3fa8888ffcb867b8
+ <+>
2023-09-16 17:00:03 +0300

    any nsaved changes

---
+++ index.js
@@ -0,0 +1,14 @@
+const navLinks=document.querySelector(".nav-link");
+
+
+function changeColorWhenClicked (e) {
+    if (e.type==="click")
+    {
+        navLinks.c
+    }
+}
+
+for (let i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener('click', changeColorWhenClicked );
+}
+
